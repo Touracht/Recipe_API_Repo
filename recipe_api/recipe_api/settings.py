bab0792c1d638ed4f3d937b989c5df3336ddd78a
Touracht <tebogomataba6810@gmail.com>
2024-10-09 19:07:39 +0200

    Switch database to sqlite

--- recipe_api/recipe_api/settings.py
+++ recipe_api/recipe_api/settings.py
@@ -83,11 +83,8 @@ WSGI_APPLICATION = 'recipe_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipe_db',  
-        'USER': 'postgres',  
-        'PASSWORD': '2023202425',  
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
